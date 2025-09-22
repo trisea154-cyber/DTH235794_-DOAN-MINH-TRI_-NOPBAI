0144a726698cad442c158f22db22c7feeecd8e15
trisea154 <trisea154@gmail.com>
2025-09-22 10:12:15 +0700

    NopBaiTapChuong2

---
+++ Chuong2_Cay5.py
@@ -0,0 +1,24 @@
+'''
+Trong Python, có hai loại ghi chú chính:
+
+1. Ghi chú một dòng:
+Khi muốn giải thích một câu lệnh hoặc đoạn mã, ta sử dụng dấu # ở đầu dòng. Tất cả những gì nằm sau dấu # sẽ không được Python thực thi, chỉ có tác dụng như một ghi chú giải thích.
+
+Ví dụ:
+
+# Đây là một ghi chú một dòng
+x = 5  # Gán giá trị 5 cho biến x
+
+
+2.Ghi chú nhiều dòng:
+Nếu cần ghi chú dài hoặc giải thích một đoạn mã phức tạp, ta có thể sử dụng dấu nháy ba (''' hoặc """). Python sẽ bỏ qua tất cả những gì nằm giữa các dấu nháy này, giúp ta ghi chú thoải mái hơn.
+
+Ví dụ:
+
+('''
+''')
+Đây là ghi chú dài, có thể giải thích nhiều phần của mã.
+Sử dụng ghi chú nhiều dòng giúp mã của mình dễ hiểu hơn.
+
+
+'''
