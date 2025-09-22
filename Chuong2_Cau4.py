254908aba79be0d8144df8df0edd1bd78e8bba40
Truong <truong_dth235803@student.agu.edu.vn>
2025-09-22 11:19:23 +0700

    NopBaiTapChuong2

--- Chuong2_Cau4.py
+++ Chuong2_Cau4.py
@@ -3,13 +3,13 @@
 
 Kiểu int dùng để lưu trữ các số nguyên.
 
-Ví dụ: x = 5, y = -10.
+Ví dụ: x = 5, y = -10
 
 2. Số thực (Floating-point number):
 
 Kiểu float dùng để lưu trữ các số có phần thập phân.
 
-Ví dụ: pi = 3.14, height = 5.8.
+Ví dụ: pi = 3.14, height = 5.8
 
 3. Chuỗi (String):
 
@@ -19,9 +19,9 @@ Ví dụ: name = "Alice", greeting = 'Hello, World!'.
 
 4. Boolean (Giá trị logic):
 
-Kiểu bool chỉ có hai giá trị là True hoặc False.
+Kiểu bool chỉ có hai giá trị là True hoặc False
 
-Ví dụ: is_active = True, is_valid = False.
+Ví dụ: is_active = True, is_valid = False
 
 6. Danh sách (List):
 
